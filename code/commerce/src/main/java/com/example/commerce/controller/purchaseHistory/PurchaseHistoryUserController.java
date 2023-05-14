c4b45b5060df67620c513c18b016b3346356db35
minhan1410 <minhan14102001@gmail.com>
2023-05-14 23:27:34 +0700

    14/05

--- code/commerce/src/main/java/com/example/commerce/controller/purchaseHistory/PurchaseHistoryUserController.java
+++ code/commerce/src/main/java/com/example/commerce/controller/purchaseHistory/PurchaseHistoryUserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
-public class BillUserController {
+public class PurchaseHistoryUserController {
     private final UserService userService;
     private final BillService billService;
     private final CouponService couponService;
